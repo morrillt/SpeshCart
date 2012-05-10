2e8bce923138da64e1b9eed8e48bda4c69932b71
Todd Morrill <Todd.Morrill@staunchrobots.com>
2012-05-10 12:47:08 -0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpeshCart::Application.config.secret_token = '03906e67343d190b820ce8361f1421367aa8c641ab745fb2551eebb9ec51179d5bc3a4182ddb67b8b7892d7cfad1b3be7e70dd007f886ba63cfde0e683c3ebb8'
